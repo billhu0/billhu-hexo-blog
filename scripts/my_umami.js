3976bfbcdc766c52dfd8fa32ca44dc21b2b928b4
billhu0 <shhuluojia@163.com>
2023-10-08 22:43:24 +0800

    new style

--- scripts/my_umami.js
+++ scripts/my_umami.js
@@ -19,3 +19,13 @@ hexo.extend.injector.register(
     `<meta name="google-site-verification" content="IqNfw3GiMxuhw7kYoEdhMJoh3j99KHuGI9bw00hPn2c" />`,
     'default'
 );
+
+// Custom font
+hexo.extend.injector.register(
+    'head_end',
+    `<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'></link>`,
+    'default'
+);
